7220a0190d517f40ea33371e5fb42d9b172d4d04
David Dai <d4dai@ucsd.edu>
2019-10-20 21:39:04 -0700

    first commit

---
+++ ICADeap.m
@@ -0,0 +1,29 @@
+filespath = dir('/Users/apple/Desktop/eeglab14_1_2b/DEAP/*.mat');
+disp(filespath)
+%EEG = eeglab();
+DEAP = 'DEAP ';
+ICA = ' ICA';
+save_dir = '/Users/apple/Desktop/eeglab14_1_2b/DEAP_ICA/';
+post_filename = '.set';
+disp(numel(filespath))
+
+for k = 1:(numel(filespath))
+    Directory = '/Users/apple/Desktop/eeglab14_1_2b/DEAP/';
+    filename = strcat(Directory, filespath(k).name)
+    EEG = pop_importdata('dataformat','matlab','nbchan',32,'data',filename,'srate',128,'pnts',0,'xmin',0,'chanlocs','/Users/apple/Desktop/eeglab14_1_2b/DEAP.ced');
+    num = num2str(k)
+    cur_setname = strcat(DEAP, num, ICA);
+    EEG.setname= cur_setname;
+    EEG = eeg_checkset( EEG );
+    EEG = eeg_checkset( EEG );
+    EEG = pop_runica(EEG, 'extended',1,'interupt','on','pca',31);
+    for i = 1:40
+        disp(i)
+        EEG.data(:,:,i) = EEG.icawinv*EEG.icaact(:,:,i)
+    end;
+    EEG = eeg_checkset( EEG );
+    EEG = eeg_checkset( EEG );
+    save_name = strcat(DEAP, num, post_filename);
+    EEG = pop_saveset( EEG, 'filename', save_name,'filepath',save_dir);
+    EEG = eeg_checkset( EEG );
+end; 
